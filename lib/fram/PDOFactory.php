8e219ed35651b1030ec21df91f02ce0ea54e1406
thomasSDev <30349391+thomasSDev@users.noreply.github.com>
2018-11-15 22:24:15 +0100

    Update PDOFactory.php

--- lib/fram/PDOFactory.php
+++ lib/fram/PDOFactory.php
@@ -5,7 +5,7 @@ class PDOFactory
 {
   public static function getMysqlConnexion()
   {
-    $db = new \PDO('mysql:host=db714842194.db.1and1.com;dbname=db714842194', 'dbo714842194', 'Maguer123456789/');
+    $db = new \PDO('mysql:host=db714842194.db.1and1.com;dbname=db714842194', 'dbo714842194', '');
     $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     
     return $db;
